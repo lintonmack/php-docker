24c92d4e30edacc8579014e4c20a09fcdcd5df38
lintonmack <lintonmackereth@gmail.com>
2020-02-09 16:37:40 +0000

    basic file structure added

---
+++ app/home.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>UK Home Swap – Free Home Swap Listing | Housing Help</title>
+  <meta name="title" content="UK Home Swap – Free Home Swap Listing | Housing Help"/>
+  <meta name="description" content="Housing Help is the Uk's #1 Free Council House Exchange Website. List your Council Home Swap Advert for free and within minutes on Housing Help."/>
+  <meta name="keywords" content="council house exchange, free council house exchange, council exchange, free council house exchange uk, council house exchange websites, home swap, homeswapper, house exchange, housing help"/>
+  <meta name="author" content="HousingHelp"/>
+  <link rel="stylesheet" href="./style/bootstrap.min.css"/>
+</head>
+<body>
+  <h1>Tired of looking for that perfect Home Swap?</h1>
+  <p>Looking to swap your home? We are here to help! Simply register your home, find something you like, then contact the tenant to see if they are interested in swapping.</p>
+  <h2> Finding a free mutual exchange home swap doesn't have to be difficult</h2>
+
+  <a href="https://www.housinghelp.co.uk/council-house-exchange/">  council house exchange </a>
+      |  <p>can seem like an impossible task. Whether you are looking to increase in size, swap for a bigger garden or you just fancy a new start in a new town, we are here to help and this is why we have created this website to give you the tools you need to succeed on this journey.</p>
+
+    <h2> 100% Free Home Swap listings, unlike any other house swap websites</h2>
+    <br/>
+
+    <h2> Guaranteed to find your next council home swap</h2>
+</body>
+</html>
